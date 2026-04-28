e03b25f3a9d5f7261e959ed946b3aaed37368db2
alisaishizaka <ish24001@byui.edu>
2026-04-29 01:44:01 +0900

    mission2-1

--- mission/index.js
+++ mission/index.js
@@ -11,6 +11,7 @@ function changeTheme() {
         let content = document.querySelector('.content');
         content.style.backgroundColor = 'black';
         content.style.border = '1px solid white';
+        document.querySelector('#subtitle').style.color = 'rgb(176, 201, 255)';
         logo.setAttribute('src', 'images/byui-logo-white.png');
     } else {
        document.body.style.backgroundColor = 'rgb(70, 70, 148)';
@@ -18,6 +19,7 @@ function changeTheme() {
        let content = document.querySelector('.content');
        content.style.backgroundColor = 'white';
        content.style.border = '1px solid black';
+       document.querySelector('#subtitle').style.color = 'rgb(0, 53, 200)';
        logo.setAttribute('src', 'images/byui-logo-blue.webp');
     }
 }
